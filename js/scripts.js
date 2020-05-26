8b531d8b3327d29b368701792d0ea402ce5da1fe
Jaime Sanchez <sanchezjaime@protonmail.com>
2020-05-25 22:02:33 -0700

    function to display employee info

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //global variables...
 const gallery = document.getElementById('gallery');
-const employees = [];
+let employees = [];
 
 /**
 *fetch functions
@@ -10,9 +10,13 @@ const url = "https://randomuser.me/api/?results=12";
 /**
 *sends a single requests to the API to fetch random users
 **/
-fetch(url)
+fetch("https://randomuser.me/api/?results=12")
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    employees = data.results
+  })
+  .then(employeeList => console.log(employees))
+
 
 /**
 *function to display random users
@@ -29,7 +33,7 @@ function createCards(data) {
 
   const img = document.createElement("img");
   img.setAttribute("class", "card-img");
-  img.setAttribute("src", "https://placehold.it/90x90");
+  img.setAttribute("src", `${employees[0].picture.large}`);
   img.setAttribute("alt", "profile picture");
   cardContainer.appendChild(img);
 
@@ -38,9 +42,9 @@ function createCards(data) {
   card.appendChild(cardInfo);
 
   cardInfo.innerHTML = `
-  <h3 id="name" class="card-name cap">first last</h3>
-  <p class="card-text">email</p>
-  <p class="card-text cap">city, state</p>
+  <h3 id="name" class="card-name cap">${employees[0].name.first} ${employees[0].name.last}</h3>
+  <p class="card-text">${employees[0].email}</p>
+  <p class="card-text cap">${employees[0].location.city}, ${employees[0].location.state}</p>
   `
 
 }
