3a12bcf3f7941ab1138fab067b94ba5754ef26a9
Jaime Sanchez <sanchezjaime@protonmail.com>
2020-05-27 22:31:48 -0700

    updated function that creates cards

--- js/scripts.js
+++ js/scripts.js
@@ -10,41 +10,47 @@ const url = "https://randomuser.me/api/?results=12";
 /**
 *sends a single requests to the API to fetch random users
 **/
-fetch("https://randomuser.me/api/?results=12")
+fetch(url)
   .then(response => response.json())
   .then(data => {
     employees = data.results
   })
-  .then(employeeList => console.log(employees))
+  .then(employeeCards => createCards(employees))
+  .catch(error => console.log("Something went wrong", error))
+
+
+
 
 
 /**
 *function to display random users
 **/
 function createCards(data) {
+  for (let i = 0; i < employees.length; i++) {
 
-  const card = document.createElement("div");
-  card.setAttribute("class", "card");
-  gallery.appendChild(card);
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    gallery.appendChild(card);
 
-  const cardContainer = document.createElement("div");
-  cardContainer.setAttribute("class", "card-img-container");
-  card.appendChild(cardContainer);
+    const cardContainer = document.createElement("div");
+    cardContainer.setAttribute("class", "card-img-container");
+    card.appendChild(cardContainer);
 
-  const img = document.createElement("img");
-  img.setAttribute("class", "card-img");
-  img.setAttribute("src", `${employees[0].picture.large}`);
-  img.setAttribute("alt", "profile picture");
-  cardContainer.appendChild(img);
+    const img = document.createElement("img");
+    img.setAttribute("class", "card-img");
+    img.setAttribute("src", `${employees[i].picture.large}`);
+    img.setAttribute("alt", "profile picture");
+    cardContainer.appendChild(img);
 
-  const cardInfo = document.createElement("div");
-  cardInfo.setAttribute("class", "card-info-container");
-  card.appendChild(cardInfo);
+    const cardInfo = document.createElement("div");
+    cardInfo.setAttribute("class", "card-info-container");
+    card.appendChild(cardInfo);
 
-  cardInfo.innerHTML = `
-  <h3 id="name" class="card-name cap">${employees[0].name.first} ${employees[0].name.last}</h3>
-  <p class="card-text">${employees[0].email}</p>
-  <p class="card-text cap">${employees[0].location.city}, ${employees[0].location.state}</p>
-  `
+    cardInfo.innerHTML = `
+    <h3 id="name" class="card-name cap">${employees[i].name.first} ${employees[i].name.last}</h3>
+    <p class="card-text">${employees[i].email}</p>
+    <p class="card-text cap">${employees[i].location.city}, ${employees[i].location.state}</p>
+    `
+  }
 
 }
