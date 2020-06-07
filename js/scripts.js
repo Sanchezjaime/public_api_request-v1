17bb976447974194a01f3a03480093287c05c942
Jaime Sanchez <sanchezjaime@protonmail.com>
2020-06-07 13:42:07 -0700

    create modal function and event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 //global variables...
 const gallery = document.getElementById('gallery');
+const body = document.querySelector('body');
+
+const closeBtn = document.getElementById('modal-close-btn');
 let employees = [];
 
 /**
@@ -15,7 +18,7 @@ fetch(url)
   .then(data => {
     employees = data.results
   })
-  .then(employeeCards => createCards(employees))
+  .then(createCard => createCards(employees))
   .catch(error => console.log("Something went wrong", error))
 
 
@@ -52,5 +55,53 @@ function createCards(data) {
     <p class="card-text cap">${employees[i].location.city}, ${employees[i].location.state}</p>
     `
   }
+  
+  const cards = document.querySelectorAll('card');
+  cards.addEventListener("click", (e) => {
+    let target = event.target;
+    console.log(target);
+    //createModal(target);
+  })
+
+}
+
+/**
+*event listener to open modal
+**/
+
+
+
+
+
+/**
+*creates modal and displays employees info
+**/
+function createModal(data) {
+
+
+  const modalContainer = document.createElement("div");
+  modalContainer.setAttribute("class", "modal-container");
+  gallery.after(modalContainer);
+
+
+  const modal = document.createElement("div");
+  modal.setAttribute("class", "modal");
+  modal.innerHTML = '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>"';
+  modalContainer.appendChild(modal);
+
+  const modalInfo = document.createElement("div");
+  modalInfo.setAttribute("class", "modal-info-container");
+  modal.appendChild(modalInfo);
+  modalInfo.innerHTML = `
+  <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+  <h3 id="name" class="modal-name cap">${employees[i].name.first} ${employees[i].name.last}</h3>
+  <p class="modal-text">email</p>
+  <p class="modal-text cap">city</p>
+  <hr>
+  <p class="modal-text">(555) 555-5555</p>
+  <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+  <p class="modal-text">Birthday: 10/21/2015</p>
+  `
+  const modalDisplay = document.querySelector("modalContainer");
 
 }
